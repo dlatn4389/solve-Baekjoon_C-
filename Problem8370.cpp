52660d7e06f5ff09fcea7fd0d61b9d51f6f70c10
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2021-01-05 04:32:24 +0900

    Plane

---
+++ Problem8370.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int n1, k1, n2, k2;
+    std::cin >> n1;
+    std::cin >> k1;
+    std::cin >> n2;
+    std::cin >> k2;
+    std::cout << n1*k1 + n2*k2 << std::endl;
+}
