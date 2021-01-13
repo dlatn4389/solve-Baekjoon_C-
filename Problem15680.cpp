349dfa931ed18fd227cb0a6d9467f6ae107ef5c3
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2021-01-13 10:23:17 +0900

    연세대학교

---
+++ Problem15680.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int N;
+    std::cin >> N;
+
+    if (N==0) {
+        std::cout << "YONSEI";
+    }
+    else if (N==1) {
+        std::cout << "Leading the Way to the Future";
+    }
+}
