c833f050573def4983ba1059b64555e64a1ad101
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2021-01-16 21:57:26 +0900

    별 찍기 - 5

---
+++ Problem2442.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<2*N-1; j++) {
+            if (N-1-i<=j && j<=N-1+i) {
+                cout << "*";
+                if (j==N-1+i) {
+                    break;
+                }
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << '\n';
+    }
+}
