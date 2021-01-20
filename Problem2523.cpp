ac3743361a78e0cd1e0bd14f9a6846c7a6cef2fb
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2021-01-20 15:44:28 +0900

    별 찍기 - 13

---
+++ Problem2523.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N; j++) {
+            if (j<=i) {
+                cout << "*";
+            }
+            else {
+                break;
+            }
+        }
+        cout << '\n';
+    }
+    for (int i=0; i<N; i++) {
+        if (i==0) {
+            continue;
+        }
+        for (int j=0; j<N; j++) {
+            if (j<=N-1-i) {
+                cout << "*";
+            }
+            else {
+                break;
+            }
+        }
+        cout << '\n';
+    }
+}
