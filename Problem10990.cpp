7188b521ef8cef4854de712b0a9628c3447754e4
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2021-01-21 16:18:05 +0900

    별 찍기 - 15

---
+++ Problem10990.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<2*N-1; j++) {
+            if (j==N-1-i||j==N-1+i) {
+                cout << "*";
+            }
+            else if (j==N+i) {
+                break;
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << '\n';
+    }
+}
