23ace07c1d3d743de46487d1627ac0a9c51dea56
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2021-01-24 06:22:01 +0900

    세 수

---
+++ Problem2985.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    if ((A+B)==C) {
+        cout << A << "+" << B << "=" << C;
+    }
+    else if ((A-B)==C) {
+        cout << A << "-" << B << "=" << C;
+    }
+    else if ((A*B)==C) {
+        cout << A << "*" << B << "=" << C;
+    }
+    else if ((A/B)==C) {
+        cout << A << "/" << B << "=" << C;
+    }
+    else if (A==(B-C)) {
+        cout << A << "=" << B << "-" << C;
+    }
+    else if (A==(B/C)) {
+        cout << A << "=" << B << "/" << C;
+    }
+}
