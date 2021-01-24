8362dd5ceb8be56edc4849859cb1e73d95205a93
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2021-01-24 09:16:40 +0900

    세 수

---
+++ Problem10817.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int nums[3];
+    for (int i=0; i<3; i++) {
+        cin >> nums[i];
+    }
+    sort(nums, nums+3);
+    cout << nums[1];
+}
