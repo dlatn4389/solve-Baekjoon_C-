f7d45d1de314f030fc1c0bb2e00188ae2944c284
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2021-01-24 09:12:19 +0900

    영수증

---
+++ Problem5565.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int total;
+    cin >> total;
+    int price;
+
+    for (int i=0; i<9; i++) {
+        cin >> price;
+        total -= price;
+    }
+
+    cout << total;
+}
