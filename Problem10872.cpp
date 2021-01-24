ca6f80e0ef03be8e3027c913a30599124f974312
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2021-01-24 10:27:08 +0900

    팩토리얼

---
+++ Problem10872.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    long long answer = 1;
+    for (int i=1; i<=N; i++) {
+        answer *= i;
+    }
+    cout << answer;
+}
