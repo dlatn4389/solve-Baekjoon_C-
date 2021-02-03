066a98876699c0f239726446fb56703b0ac9e935
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2021-02-03 11:59:50 +0900

    그대로 출력하기 2

---
+++ Problem11719.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    string input;
+    while (getline(cin, input)) {
+        cout << input << '\n';
+    }
+}
