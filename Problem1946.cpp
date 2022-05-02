fcbe0ee53b34ee7c8a114e341521ef2a2ed0e743
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2022-05-02 15:47:29 +0900

    신입 사원

---
+++ Problem1946.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 테스트케이스의 개수
+    int t; cin >> t;
+
+    while (t--) {
+        // 지원자의 숫자
+        int n; cin >> n;
+        
+    }
+}
